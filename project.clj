8f8a7aea8d81353cc9d6f06b428b42e6aae9a37f
Duncan McGreggor <oubiwann@gmail.com>
2016-11-17 16:47:43 -0600

    Bumped version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojusc/cljs-tools "0.1.0"
+(defproject clojusc/cljs-tools "0.1.1"
   :description "Useful functions for cljs-based projects"
   :url "https://github.com/clojusc/cljs-tools"
   :license
