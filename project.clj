32e3fcbcbf389c7b0f9bbda9097c410148b95d99
Duncan McGreggor <oubiwann@gmail.com>
2017-10-11 01:36:18 -0500

    Updated deps and aliases.

--- project.clj
+++ project.clj
@@ -7,9 +7,9 @@
   :dependencies [
     [joda-time/joda-time "2.9.9"]
     [org.clojure/clojure "1.8.0"]
-    [org.clojure/clojurescript "1.9.542"]]
+    [org.clojure/clojurescript "1.9.946"]]
   :plugins [
-    [lein-cljsbuild "1.1.6"]]
+    [lein-cljsbuild "1.1.7"]]
   :clean-targets ^{:protect false}
     ["resources/public/js"
      "target"]
@@ -41,21 +41,40 @@
       ^{:doc "Start a browser-based Clojurescript REPL"}
       ["trampoline" "run" "-m" "clojure.main"
        "dev-resources/scripts/browser-repl.clj"]
-     }
+    "check-deps" [
+      "with-profile" "+test" "ancient" "check" ":all"]
+    "kibit" [
+      "with-profile" "+test" "do"
+        ["shell" "echo" "== Kibit =="]
+        ["kibit"]]
+    "outlaw" [
+      "with-profile" "+test"
+      "eastwood" "{:namespaces [:source-paths] :source-paths [\"src\"]}"]
+    "lint" [
+      "with-profile" "+test" "do"
+        ["check"] ["kibit"] ["outlaw"]]
+    "build"
+      ^{:doc "Perform build steps."}
+      ["do" ["check"] ["compile"] ["with-profile" "+ubercompile" "compile"]]}
   :profiles {
-    :uberjar {
+    :ubercompile {
       :aot :all}
     :test {
-      :plugins [
-        [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-        [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
-        [lein-ancient "0.6.10"]]
       :test-selectors {
         :default :unit
         :unit :unit
         :system :system
         :integration :integration}
-      :source-paths ["test/clj"]}
+      :source-paths ["test/clj"]
+      :dependencies [
+        [org.clojure/tools.namespace "0.2.11"]]
+      :plugins [
+        [jonase/eastwood "0.2.4"]
+        [lein-ancient "0.6.12"]
+        [lein-bikeshed "0.4.1"]
+        [lein-kibit "0.1.5"]
+        [lein-shell "0.5.0"]
+        [venantius/yagni "0.1.4"]]}
     :dev {
       :source-paths ["dev-resources/src"]
       :repl-options {:init-ns clojusc.cljs-tools.dev}
